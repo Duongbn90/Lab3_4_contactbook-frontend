ec8b0ee3b738202ab2e3b112919b7fbbb2ae81a1
Duongbn <nhieuduong17690@gmail.com>
2022-07-10 13:36:04 +0700

    Khoi tao du an

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<!--<script setup="setup">
+    import HelloWorld from './components/HelloWorld.vue'
+    import TheWelcome from './components/TheWelcome.vue'
+</script>-->
+
+<script>
+    export default {}
+</script>
+<template>
+    <h1>Hello, Vue.js!</h1>
+</template>
+<style>
+    .page {
+        max-width: 400px;
+        margin: auto;
+    }
+</style>
